7fe545e9fd18cb76d6b0d7ed9dba7623789ae877
idanlo <idan2110@gmail.com>
2018-12-07 15:59:20 +0200

    Changes to placing markers

--- client.py
+++ client.py
@@ -25,126 +25,89 @@ def player_input():
     else:
         return ("X", "O")
 
-def place_marker(board, marker, position):
-    board[position] = marker
-
-def win_check(board, marker):
-    if board[7] == marker and board[8] == marker and board[9] == marker:
-        return True
-    if board[4] == marker and board[5] == marker and board[6] == marker:
-        return True
-    if board[1] == marker and board[2] == marker and board[3] == marker:
-        return True
-    if board[7] == marker and board[4] == marker and board[1] == marker:
-        return True
-    if board[8] == marker and board[5] == marker and board[2] == marker:
-        return True
-    if board[9] == marker and board[6] == marker and board[3] == marker:
-        return True
-    if board[7] == marker and board[5] == marker and board[3] == marker:
-        return True
-    if board[9] == marker and board[5] == marker and board[1] == marker:
-        return True
-
-    return False
-
-def choose_first():
-    if random.randint(0,1) == 1:
-        return "Player 1"
-    else:
-        return "Player 2"
-
-def space_check(board, position):
-    if board[position] == " ":
-        return True
-    else:
-        return False
-
 def full_board_check(board):
     for x in board:
         if x == " ":
             return False
     return True
 
-def player_choice(board):
+def player_choice():
     position = ""
-    while position not in '1 2 3 4 5 6 7 8 9'.split() or not space_check(board, int(position)):
+    while position not in '1 2 3 4 5 6 7 8 9'.split():
         position = raw_input("Choose your next position: (1 - 9) ")
-    return int(position)
+    return position
 
-def replay():
-    replay_string = raw_input("Do you want to play again? (Enter Yes or No) ").upper()
-    if replay_string[0] == "Y":
-        return True
-    else: 
-        return False
 
 def main():
     print "Welcome to Tic Tac Toe!"
+    game_board = ["0"," "," "," "," "," "," "," "," "," "]
+    player1_marker, player2_marker = player_input()
+    turn = "Player 1"
+    game_on = False
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("127.0.0.1", 8080))
+    my_id = ""
+
+    sock.send("NEW_PLAYER")
+    my_id = sock.recv(1024)
+    print my_id
+    is_user_turn = False
+    connected_to_lobby = False
+    board = []
     while True:
-        game_board = ["0"," "," "," "," "," "," "," "," "," "]
-        player1_marker, player2_marker = player_input()
-        turn = "Player 1"
-        game_on = False
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        sock.connect(("127.0.0.1", 8080))
-        my_id = ""
+        data = ""
+        while not connected_to_lobby:
+            lobby_id = raw_input("Enter lobby id: ")
+            sock.send("JOIN_LOBBY " + lobby_id)
+            data = sock.recv(1024)
+            print data
+            data = data.split()
+            print data[0] != "FAILED_TO_JOIN_LOBBY"
+            if data[0] != "FAILED_TO_JOIN_LOBBY":
+                connected_to_lobby = True
+        # display initial board
+        sock.send("GET_BOARD")
+        board = sock.recv(1024).split("*")
+        display_board(board)
+        # wait until it is the users turn
+        data = ""
+        while data != my_id:
+            sock.send("GET_CURRENT_TURN")
+            data = sock.recv(1024)
+        # display board again if there were any changes
+        sock.send("GET_BOARD")
+        board = sock.recv(1024).split("*")
+        display_board(board)
+        # position is a number to index the board array
+        err = True
+        while err:
+            position = player_choice()
+            sock.send("PLACE_MARKER " + position)
+            data = sock.recv(1024).split()
+            if data[0] == "OK":
+                err = False
+            elif data[0] == "PLAYER_WON":
+                print " ".join(data[1:])
+        # break
 
-        sock.send("NEW_PLAYER")
-        my_id = sock.recv(1024)
-        print my_id
-        connected_to_lobby = False
-        board = []
-        while not game_on:
-            data = ""
-            while not connected_to_lobby:
-                lobby_id = raw_input("Enter lobby id: ")
-                sock.send("JOIN_LOBBY " + lobby_id + " " + my_id)
-                data = sock.recv(1024)
-                print data
-                data = data.split()
-                print data[0] != "FAILED_TO_JOIN_LOBBY"
-                if data[0] != "FAILED_TO_JOIN_LOBBY":
-                    connected_to_lobby = True
-            sock.send("GET_BOARD " + my_id)
-                
 
-
-        while game_on:
-            if turn == "Player 1":
-                display_board(game_board)
-                position = player_choice(game_board)
-                place_marker(game_board, player1_marker, position)
-                
-                if win_check(game_board, player1_marker):
-                    display_board(game_board)
-                    print "Player 1 Won! "
-                    game_on = False
-                else:
-                    if full_board_check(game_board):
-                        display_board(game_board)
-                        print "The game is a draw! "
-                        break
-                    else:
-                        turn = "Player 2"
-            elif turn == "Player 2":
-                display_board(game_board)
-                position = player_choice(game_board)
-                place_marker(game_board, player2_marker, position)
-                
-                if win_check(game_board, player2_marker):
-                    display_board(game_board)
-                    print "Player 2 Won! "
-                    game_on = False
-                else:
-                    if full_board_check(game_board):
-                        display_board(game_board)
-                        print "The game is a draw! "
-                        break
-                    else:
-                        turn = "Player 1"
-        if not replay():
-            break
+    # while game_on:
+    #     if turn == "Player 1":
+    #         display_board(game_board)
+    #         position = player_choice(game_board)
+    #         place_marker(game_board, player1_marker, position)
+            
+    #         if win_check(game_board, player1_marker):
+    #             display_board(game_board)
+    #             print "Player 1 Won! "
+    #             game_on = False
+    #         else:
+    #             if full_board_check(game_board):
+    #                 display_board(game_board)
+    #                 print "The game is a draw! "
+    #                 break
+    #             else:
+    #                 turn = "Player 2"
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -3,61 +3,116 @@ import random
 import uuid
 import thread
 
-class Game(object):
+class Lobby(object):
     def __init__(self, id):
         self.board = ["0"," "," "," "," "," "," "," "," "," "]
         self.turn = "Player 1" if random.randint(0, 1) == 1 else "Player 2"
         self.id = id
         self.playercount = 1
+        self.current_turn = None
+        self.players = []
 
-    
+
     def __str__(self):
         return "*".join(self.board)
 
-class Player(object):
-    def __init__(self, id):
-        self.id = id
 
-    def join_lobby(self, lobbyid):
-        self.lobbyid = lobbyid
+class Player(object):
+    def __init__(self, id, sock):
+        self.id = id
+        self.lobby = None
+        self.marker = "X"
+        self.sock = sock
+
+    def join_lobby(self, lobby):
+        self.lobby = lobby
+
+def win_check(board, marker):
+    if board[7] == marker and board[8] == marker and board[9] == marker:
+        return True
+    if board[4] == marker and board[5] == marker and board[6] == marker:
+        return True
+    if board[1] == marker and board[2] == marker and board[3] == marker:
+        return True
+    if board[7] == marker and board[4] == marker and board[1] == marker:
+        return True
+    if board[8] == marker and board[5] == marker and board[2] == marker:
+        return True
+    if board[9] == marker and board[6] == marker and board[3] == marker:
+        return True
+    if board[7] == marker and board[5] == marker and board[3] == marker:
+        return True
+    if board[9] == marker and board[5] == marker and board[1] == marker:
+        return True
+
+    return False
 
 def on_new_client(client_socket, address):
     
+    global lobbies
+    global players
+    player_id = ""
     while True:
         data = client_socket.recv(1024)
         data = data.split()
         if data[0] == "NEW_PLAYER":
-            new_id = str(uuid.uuid4())
-            players.append(Player(new_id))
-            client_socket.send(new_id)
+            player_id = str(uuid.uuid4())
+            players.append(Player(player_id, client_socket))
+            client_socket.send(player_id)
             continue
-        # if the request isnt new player then one of the arguments must have the player id
-        if data[2] in [player.id for player in players]:
-            if data[0] == "JOIN_LOBBY":
-                if len(data) == 3:
+        if len(player_id) > 0:
+            player = [x for x in players if x.id == player_id]
+            if len(player) > 0:
+                player = player[0]
+                if data[0] == "JOIN_LOBBY":
                     lob_id = data[1]
+                    # find lobby with id lob_id
                     lob = [x for x in lobbies if x.id == lob_id]
+
                     if len(lob) > 0:
                         if lob[0].playercount < 2:
                             lob[0].playercount += 1
-                            print "Player (id: " + data[2] + ") joined lobby (id: " + lob_id + ")"
-                            print lob[0]
+                            player.lobby = lob[0]
+                            player.marker = "O"
+                            lob[0].players.append(player)
+                            print "Player (id: " + player_id + ") joined lobby (id: " + lob_id + ")"
                             client_socket.send("JOIN_LOBBY " + lob_id)
                         else:
-                            print "Player (id: " + data[2] + ") attempted to join lobby (id: " + lob_id + ") but it is full"
+                            print "Player (id: " + player_id + ") attempted to join lobby (id: " + lob_id + ") but it is full"
                             client_socket.send("FAILED_TO_JOIN_LOBBY there are already 2 players")
                     else:
-                        lobbies.append(Game(lob_id))
-                        print "New lobby created (id: " + lob_id + ") with 1 player (id: " + data[2] + ")"
+                        new_lobby = Lobby(lob_id)
+                        new_lobby.current_turn = player
+                        new_lobby.players.append(player)
+                        lobbies.append(new_lobby)
+                        player.lobby = new_lobby
+                        print "New lobby created (id: " + lob_id + ") with 1 player (id: " + player_id + ")"
                         client_socket.send("CREATED_LOBBY " + lob_id)
-            
-            # p_id = data[0]
-            # if data[0] not in [player.id for player in players]:
-            #     players.append(Player(data[0]))
-            # if data[0] == "NEW_LOBBY":
-            #     lobby_id = int(data[1])
-            #     lobby = [lob for lob in lobbies if lob.id == lobby_id]
-            #     if len(lobby) > 0 and lobby[0].playercount < 2:
+                elif data[0] == "GET_BOARD":
+                    client_socket.send(str(player.lobby))
+                elif data[0] == "GET_CURRENT_TURN":
+                    client_socket.send(player.lobby.current_turn.id)
+                elif data[0] == "PLACE_MARKER":
+                    pos = int(data[1])
+                    if player.lobby.board[pos] == " " and player.lobby.current_turn == player:
+                        player.lobby.board[pos] = player.marker
+                        # set turn to other player
+                        if player.lobby.players[0] == player:
+                            player.lobby.current_turn = player.lobby.players[1]
+                        else:
+                            player.lobby.current_turn = player.lobby.players[0]
+                        has_won = win_check(player.lobby.board, player.marker)
+                        if has_won:
+                            for p in player.lobby.players:
+                                print p
+                                p.sock.send("PLAYER_WON PLAYER " + player.id + " HAS WON")
+                        else:
+                            client_socket.send("OK")
+                    else:
+                        client_socket.send("ERROR")
+            else:
+                print "User not found"
+                client_socket.send("FAILED_TO_JOIN_LOBBY User not found")
 
 
 def main():
